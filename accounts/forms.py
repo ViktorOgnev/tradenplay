9c64a75bc15abd340673ab78dcce49226973aa75
Viktor Ognev <ognev.victor@gmail.com>
2013-09-11 12:05:57 +0300

    Create locale translation strings for russian.

--- accounts/forms.py
+++ accounts/forms.py
@@ -17,20 +17,18 @@ class RegistrationForm(UserCreationForm):
     password1 = forms.RegexField(
         label=_("Password"),
         regex=ur'^(?=.*\W+).*$',
-        help_text=_(
-            "Password must be 6 characters long and contain \
-            at least one symbol that is not letter or number."
-        ),
+        help_text=_("Password must be 6 characters long and contain" 
+                    "at least one symbol that is not letter or number."
+                    ),
         widget=forms.PasswordInput(render_value=False),
         min_length=6,
     )
     password2 = forms.RegexField(
         label=_("Password confirmation"),
         regex=ur'^(&=.*\W+).$',
-        help_text=_(
-            "Please reenter your password manually, without copying  \
-            to prevent wrong password input"
-        ),
+        help_text=_("Please reenter your password manually, without copying"
+                    "to prevent wrong password input."
+                    ),
         widget=forms.PasswordInput(render_value=False),
         min_length=6,
     )
